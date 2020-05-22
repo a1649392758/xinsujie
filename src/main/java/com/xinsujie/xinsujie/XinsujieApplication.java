1b9ac8e135adf3661b43e3b921a4040ae9c365d5
a1649392758 <1649392758@qq.com>
2020-05-22 10:54:49 +0800

    第一版demo

---
+++ src/main/java/com/xinsujie/xinsujie/XinsujieApplication.java
@@ -0,0 +1,14 @@
+package com.xinsujie.xinsujie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XinsujieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XinsujieApplication.class, args);
+        System.out.println("test");
+    }
+
+}
---
+++ src/test/java/com/xinsujie/xinsujie/XinsujieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinsujie.xinsujie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XinsujieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
